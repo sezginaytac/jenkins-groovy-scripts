bf801de85cc4d2d619dab90ce2a2c210cce46b61
sezginaytac <sezginaytac@yahoo.com>
2018-07-11 06:18:28 +0000

    Update avgBuildDuration.groovy
    added the the version using the inject() method

--- avgBuildDuration.groovy
+++ avgBuildDuration.groovy
@@ -1,5 +1,11 @@
 def job = Hudson.instance.items.find{ it.name == 'xxxxx' }
 
+// with the spread-dot operator (*.) 
 def totalDurationInSeconds = (int) (job.builds*.duration.sum() / 1000 / 60)
 def avgDurationInSeconds = totalDurationInSeconds / job.builds.size()
 println avgDurationInSeconds
+
+// with the inject() method
+def totalDurationInSeconds2 = (int) (job.builds.inject(0){result, b -> result += b.duration} / 1000 / 60)
+def avgDurationInSeconds2 = totalDurationInSeconds2 / job.builds.size()
+println avgDurationInSeconds2
